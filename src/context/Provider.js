78c34331f644a7a3e59c751a04eecce0d997e18f
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-31 21:50:08 -0300

    Mock de musica identificada no provider

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import AppContext from "./AppContext";
 
 export default function Provider({ children }) {
-  const [isRecording, setIsRecording] = useState(false);
-  const [identifiedSong, setIdentifiedSong] = useState({
+  const identifiedSongMock = {
     album: "Midnight Room",
     artist: "Springtime Carnivore",
     genre: "Alternative",
     release_year: "2016",
     title: "Bad Dream Baby",
-  });
+    artwork: "https://i.scdn.co/image/ab67616d0000b2733525ba2609c82b320a811ce9"
+  }
+
+  const [isRecording, setIsRecording] = useState(false);
+  const [identifiedSong, setIdentifiedSong] = useState();
   const [count, setCount] = useState(0);
 
   const contextValues = {
