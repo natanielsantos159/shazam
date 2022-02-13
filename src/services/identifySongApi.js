71a679e34cf9c5cb0b5365babc37356964b55067
Nataniel Santos <nathan.santos159@hotmail.com>
2022-02-12 23:23:05 -0300

    Configura vars de ambiente

--- src/services/identifySongApi.js
+++ src/services/identifySongApi.js
@@ -1,11 +1,13 @@
+
 const identifySong = async (file) => {
   const form = new FormData();
   form.append("file", file, "file.mp3");
+
   const response = await fetch("https://music-identify.p.rapidapi.com/identify", {
     method: "POST",
     headers: {
       "x-rapidapi-host": "music-identify.p.rapidapi.com",
-      "x-rapidapi-key": "fc239fa2f3msha65f422dd6b0656p159e5fjsn3dd5f36474a5",
+      "x-rapidapi-key": process.env.REACT_APP_APIKEY,
     },
     body: form,
   })
